949b8c2d853b2d7f14846b53db8d06264c5c4450
Valentyn Berekelia <yamato.watanabu@gmail.com>
2026-01-25 12:36:13 +0200

    Add PUT and PATCH method.

--- CinemaAura/AbsoluteCinema.Presentation/Controllers/MovieController.cs
+++ CinemaAura/AbsoluteCinema.Presentation/Controllers/MovieController.cs
@@ -1,8 +1,29 @@
+using AbsoluteCinema.Application.DTOs.Movie;
+using AbsoluteCinema.Application.Features.Movies.Command;
+using AbsoluteCinema.Domain.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
+using static AbsoluteCinema.Application.Features.Movies.Command.UpdateMoviePartialCommandHandler;
 
 namespace AbsoluteCinema.Controllers;
 
+[Route("api/admin")]
 public class MovieController : ControllerBase
 {
-    
+    private readonly IMediator _mediator;
+    [HttpPut]
+    [Route("/movie/{id:guid}")]
+    public async Task<IActionResult> UpdateMovieFull(Guid movieId, [FromBody] MovieUpdateRequest request, CancellationToken ct)
+    {
+        await _mediator.Send(new UpdateMovieFullCommand(new MovieId(movieId), request), ct);
+        return NoContent();
+    }
+    [HttpPatch]
+    [Route("/movie/{id:guid}")]
+    public async Task<IActionResult> UpdateMoviePartial(Guid muvieId, [FromBody] MovieUpdatePartialRequest request,CancellationToken ct)
+    {
+        await _mediator.Send(new UpdateMoviePartialCommand(new MovieId(muvieId), request), ct);
+        return NoContent();
+    }
 }
