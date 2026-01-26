e689b26e0ff469845f934b4af215bdaba7b96afc
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2026-01-26 13:37:23 +0200

    Feature/movie update (#92)
    * Add migration
    
    * Update gitignore
    
    * Add PUT and PATCH method.
    
    * Remove password from DefaultConnection string
    
    Removed the password from the DefaultConnection string in appsettings.Development.json.
    
    * Change MovieController route to api/admin

---
+++ CinemaAura/AbsoluteCinema.Application/Features/Movies/Command/UpdateMoviePartialCommandHandler.cs
@@ -0,0 +1,65 @@
+﻿using AbsoluteCinema.Application.DTOs.Movie;
+using AbsoluteCinema.Application.Repository;
+using AbsoluteCinema.Domain.Entities;
+using AbsoluteCinema.Domain.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AbsoluteCinema.Application.Features.Movies.Command.UpdateMoviePartialCommandHandler;
+
+namespace AbsoluteCinema.Application.Features.Movies.Command
+{
+    public class UpdateMoviePartialCommandHandler : IRequestHandler<UpdateMoviePartialCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMovieRepository _movies;
+        public async Task Handle(UpdateMoviePartialCommand command, CancellationToken ct)
+        {
+            var movie = await _movies.GetByIdForUpdateAsync(command.MovieId, ct);
+            if (movie is null)
+                throw new DomainException("Movie not found.");
+
+            var r = command.Request;
+
+            if (r.Name is null &&
+                r.Description is null &&
+                r.Rate is null &&
+                r.AgeLimit is null &&
+                r.DurationSeconds is null &&
+                r.Country is null &&
+                r.Studio is null &&
+                r.Language is null)
+
+            if (r.Name is not null)
+                movie.ChangeName(r.Name);
+
+            if (r.Description is not null)
+                movie.ChangeDescription(r.Description);
+
+            if (r.Rate.HasValue)
+                movie.ChangeRate(r.Rate.Value);
+
+            if (r.AgeLimit.HasValue)
+                movie.ChangeAge(r.AgeLimit.Value);
+
+            if (r.DurationSeconds.HasValue)
+                movie.ChangeDuration(TimeSpan.FromSeconds(r.DurationSeconds.Value));
+
+            if (r.Country is not null)
+                movie.ChangeCountry(r.Country);
+
+            if (r.Studio is not null)
+                movie.ChangeStudio(r.Studio);
+
+            if (r.Language is not null)
+                movie.ChangeLanguage(r.Language);
+
+            _movies.Update(movie);
+            await _unitOfWork.SaveChangesAsync(ct);
+        }
+        public record UpdateMoviePartialCommand(MovieId MovieId, MovieUpdatePartialRequest Request) : IRequest;
+    }
+}
--- CinemaAura/AbsoluteCinema.Presentation/Controllers/MovieController.cs
+++ CinemaAura/AbsoluteCinema.Presentation/Controllers/MovieController.cs
@@ -1,3 +1,6 @@
+using AbsoluteCinema.Application.DTOs.Movie;
+using AbsoluteCinema.Application.Features.Movies.Command;
+using AbsoluteCinema.Domain.Entities;
 using AbsoluteCinema.Application.Features.Movies.Queries;
 using AbsoluteCinema.Domain.Entities;
 using AbsoluteCinema.Requests;
@@ -5,9 +8,10 @@ using Mapster;
 using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
+using static AbsoluteCinema.Application.Features.Movies.Command.UpdateMoviePartialCommandHandler;
 
 namespace AbsoluteCinema.Controllers;
-
 [Route("api")]
 public class MovieController(IMediator mediator, IMapper mapper) : ControllerBase
 {
@@ -32,5 +36,18 @@ public class MovieController(IMediator mediator, IMapper mapper) : ControllerBas
         var response = await _mediator.Send(query);
         return Ok(response);
     }
-    
+    [HttpPut]
+    [Route("/admin/movie/{id:guid}")]
+    public async Task<IActionResult> UpdateMovieFull(Guid movieId, [FromBody] MovieUpdateRequest request, CancellationToken ct)
+    {
+        await _mediator.Send(new UpdateMovieFullCommand(new MovieId(movieId), request), ct);
+        return NoContent();
+    }
+    [HttpPatch]
+    [Route("/admin/movie/{id:guid}")]
+    public async Task<IActionResult> UpdateMoviePartial(Guid muvieId, [FromBody] MovieUpdatePartialRequest request,CancellationToken ct)
+    {
+        await _mediator.Send(new UpdateMoviePartialCommand(new MovieId(muvieId), request), ct);
+        return NoContent();
+    }
 }
