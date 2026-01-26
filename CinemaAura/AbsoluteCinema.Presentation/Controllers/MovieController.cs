734814d4b52a5d64555a207d071de42684b53102
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2026-01-26 13:29:25 +0200

    Change MovieController route to api/admin

--- CinemaAura/AbsoluteCinema.Presentation/Controllers/MovieController.cs
+++ CinemaAura/AbsoluteCinema.Presentation/Controllers/MovieController.cs
@@ -7,20 +7,19 @@ using System.Security.Cryptography.Xml;
 using static AbsoluteCinema.Application.Features.Movies.Command.UpdateMoviePartialCommandHandler;
 
 namespace AbsoluteCinema.Controllers;
-
-[Route("api/admin")]
+[Route("api")]
 public class MovieController : ControllerBase
 {
     private readonly IMediator _mediator;
     [HttpPut]
-    [Route("/movie/{id:guid}")]
+    [Route("/admin/movie/{id:guid}")]
     public async Task<IActionResult> UpdateMovieFull(Guid movieId, [FromBody] MovieUpdateRequest request, CancellationToken ct)
     {
         await _mediator.Send(new UpdateMovieFullCommand(new MovieId(movieId), request), ct);
         return NoContent();
     }
     [HttpPatch]
-    [Route("/movie/{id:guid}")]
+    [Route("/admin/movie/{id:guid}")]
     public async Task<IActionResult> UpdateMoviePartial(Guid muvieId, [FromBody] MovieUpdatePartialRequest request,CancellationToken ct)
     {
         await _mediator.Send(new UpdateMoviePartialCommand(new MovieId(muvieId), request), ct);
