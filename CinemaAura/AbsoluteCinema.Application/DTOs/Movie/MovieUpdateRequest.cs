ed12305f6fe5ecb690164b8595f4cf00b4a3a2ad
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2026-01-26 13:37:23 +0200

    Feature/movie update (#92)
    * Add migration
    
    * Update gitignore
    
    * Add PUT and PATCH method.
    
    * Remove password from DefaultConnection string
    
    Removed the password from the DefaultConnection string in appsettings.Development.json.
    
    * Change MovieController route to api/admin

---
+++ CinemaAura/AbsoluteCinema.Application/DTOs/Movie/MovieUpdateRequest.cs
@@ -0,0 +1,19 @@
+﻿using AbsoluteCinema.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteCinema.Application.DTOs.Movie
+{
+    public record MovieUpdateRequest(
+        string Name,
+        string Description, 
+        decimal Rate, 
+        int AgeLimit, 
+        int DurationSecond, 
+        string Country, 
+        string Studio,
+        string Language);
+}
