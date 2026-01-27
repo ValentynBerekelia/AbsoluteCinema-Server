2d16d0f0697366d0fe76bc9ef5be386c76a02f6a
Mykhailo Hordei <myhailo.gordei@gmail.com>
2026-01-27 23:01:32 +0200

    fixed migraton, last one didnt work:((

---
+++ CinemaAura/AbsoluteCinema.Infrastructure/Migrations/20260127204822_AddMovieFormatToSession.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AbsoluteCinema.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieFormatToSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Halls",
+                table: "Halls");
+
+            migrationBuilder.RenameTable(
+                name: "Halls",
+                newName: "halls");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "format",
+                table: "sessions",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_halls",
+                table: "halls",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_halls",
+                table: "halls");
+
+            migrationBuilder.DropColumn(
+                name: "format",
+                table: "sessions");
+
+            migrationBuilder.RenameTable(
+                name: "halls",
+                newName: "Halls");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Halls",
+                table: "Halls",
+                column: "id");
+        }
+    }
+}
