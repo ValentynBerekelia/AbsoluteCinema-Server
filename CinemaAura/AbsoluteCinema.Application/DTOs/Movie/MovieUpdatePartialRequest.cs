22e0d2e499e82820abb68e7df111909d8261f10f
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2026-01-27 14:58:06 +0200

    Update (#99)
    * Update
    
    * Optimize BaseRepository methods for better performance
    
    Refactor methods to use AsNoTracking and simplify queries.

--- CinemaAura/AbsoluteCinema.Application/DTOs/Movie/MovieUpdatePartialRequest.cs
+++ CinemaAura/AbsoluteCinema.Application/DTOs/Movie/MovieUpdatePartialRequest.cs
@@ -7,15 +7,14 @@ using System.Threading.Tasks;
 
 namespace AbsoluteCinema.Application.DTOs.Movie
 {
-    public sealed record MovieUpdatePartialRequest
-    {
-        public string? Name { get; init; }
-        public string? Description { get; init; }
-        public decimal? Rate { get; init; }
-        public int? AgeLimit { get; init; }
-        public int? DurationSeconds { get; init; }
-        public string? Country { get; init; }
-        public string? Studio { get; init; }
-        public string? Language { get; init; }
-    }
+    public record MovieUpdatePartialRequest(
+         string? Name,
+         string? Description,
+         decimal? Rate,
+         int? AgeLimit,
+         int? DurationSeconds,
+         string? Country,
+         string? Studio,
+         string? Language
+    );
 }
