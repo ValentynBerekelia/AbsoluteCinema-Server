ae4ad78c23b736e215f7c51cab64de66096af445
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2026-01-28 21:02:17 +0200

    fix (#123)

--- CinemaAura/AbsoluteCinema.Infrastructure/Persistence/Configurations/TypePriceConfiguration.cs
+++ CinemaAura/AbsoluteCinema.Infrastructure/Persistence/Configurations/TypePriceConfiguration.cs
@@ -43,18 +43,19 @@ public class TypePriceConfiguration : IEntityTypeConfiguration<TypePrice>
         builder.HasIndex(tp => new { tp.SessionId, tp.SeatTypeId })
             .IsUnique()
             .HasDatabaseName("uq_type_prices_session_seat_type");
-
-        builder.HasOne<Session>()
-            .WithMany()
+        ///////////////////
+        builder.HasOne(tp => tp.Session)
+            .WithMany(s => s.TypePrices)
             .HasForeignKey(tp => tp.SessionId)
             .HasConstraintName("fk_type_prices_sessions_session_id")
             .OnDelete(DeleteBehavior.Cascade);
-
-        builder.HasOne<SeatType>()
+        //////////////////////////
+        builder.HasOne(tp => tp.SeatType)
             .WithMany()
             .HasForeignKey(tp => tp.SeatTypeId)
             .HasConstraintName("fk_type_prices_seat_types_seat_type_id")
             .OnDelete(DeleteBehavior.Cascade);
+
 
         builder.HasIndex(tp => tp.SessionId)
             .HasDatabaseName("ix_type_prices_session_id");
