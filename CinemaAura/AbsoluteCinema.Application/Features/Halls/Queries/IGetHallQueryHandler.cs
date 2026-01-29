360b437d3f9af75e20e2cb25a70d768c5313a3f3
Valentyn Berekelia <yamato.watanabu@gmail.com>
2026-01-29 22:27:10 +0200

    add Holl API get method

---
+++ CinemaAura/AbsoluteCinema.Application/Features/Halls/Queries/IGetHallQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteCinema.Application.Features.Halls.Queries
+{
+    public interface IGetHallQueryHandler
+    {
+        Task<GetHallQueryResponse> ExecuteAsync(GetHallQuery request, CancellationToken ct);
+    }
+}
--- CinemaAura/AbsoluteCinema.Presentation/Controllers/HallController.cs
+++ CinemaAura/AbsoluteCinema.Presentation/Controllers/HallController.cs
@@ -1,11 +1,28 @@
+using AbsoluteCinema.Application.Features.Halls.Queries;
+using AbsoluteCinema.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AbsoluteCinema.Controllers;
-
+[Route("api")]
+[ApiController]
 public class HallController(IMediator mediator) : ControllerBase
 {
     private readonly IMediator _mediator = mediator;
+    [HttpGet]
+    [Route("halls")]
+    public async Task<IActionResult> GetHalls(CancellationToken ct)
+    {
+        var response = await _mediator.Send(new GetHallsQuery(),ct);
+        return Ok(response);
+    }
 
-    
+    [HttpGet]
+    [Route("halls/{id:guid}")]
+    public async Task<IActionResult> GetHall(Guid id,CancellationToken ct)
+    {
+        var query = new GetHallQuery(new HallId(id));
+        var response = await _mediator.Send(query, ct);
+        return Ok(response);
+    }
 }
