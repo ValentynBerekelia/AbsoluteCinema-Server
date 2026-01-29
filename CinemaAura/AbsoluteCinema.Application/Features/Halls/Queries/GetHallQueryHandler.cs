f8a37d7ea212c37041ccd8e9c7e22b54f12b4fd0
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2026-01-29 22:41:19 +0200

    add Holl API get method (#142)

---
+++ CinemaAura/AbsoluteCinema.Application/Features/Halls/Queries/GetHallQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AbsoluteCinema.Application.DTOs;
+using AbsoluteCinema.Application.Features.Movies.Queries;
+using AbsoluteCinema.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteCinema.Application.Features.Halls.Queries
+{
+    public class GetHallQueryHandler(IGetHallQueryHandler detaildQuery)
+        :IRequestHandler<GetHallQuery, GetHallQueryResponse>
+    {
+        public async Task<GetHallQueryResponse> Handle(GetHallQuery request, CancellationToken ct)
+        {
+            var hall = await detaildQuery.ExecuteAsync(request, ct);
+            if(hall is null)
+            {
+                throw new Exception($"Hall {request.HallId} Not Found");
+            }
+            return hall;
+        }
+    }
+    public record GetHallQuery : IRequest<GetHallQueryResponse>
+    {
+        public HallId HallId { get; }
+        public GetHallQuery(HallId id) => HallId = id;
+    }
+    public record GetHallQueryResponse(
+        HallId HallId,
+        string Name,
+        IEnumerable<SeatDto> Seats
+        ) { }
+}
