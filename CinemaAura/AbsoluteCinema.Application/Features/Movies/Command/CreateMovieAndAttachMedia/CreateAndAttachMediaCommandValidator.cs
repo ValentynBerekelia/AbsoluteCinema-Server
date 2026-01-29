c32a6550204e296f9869000b58c49986942e4ee2
Stanislav Malovaniuk <stasmalovaniuk@gmail.com>
2026-01-29 13:02:31 +0200

    Added new movie endpoint, a bit modify of previous (just addded dtos and other triffes), added name case insensitive for serialization

---
+++ CinemaAura/AbsoluteCinema.Application/Features/Movies/Command/CreateMovieAndAttachMedia/CreateAndAttachMediaCommandValidator.cs
@@ -0,0 +1,36 @@
+﻿using AbsoluteCinema.Domain.Enums;
+using FluentValidation;
+
+namespace AbsoluteCinema.Application.Features.Movies.Command.CreateMovieAndAttachMedia
+{
+    public class CreateAndAttachMediaCommandValidator : AbstractValidator<CreateAndAttachMediaCommand>
+    {
+        public CreateAndAttachMediaCommandValidator()
+        {
+            RuleFor(x => x.MovieId)
+                .NotEmpty()
+                .WithMessage("Movie ID is required.");
+
+            RuleFor(x => x.Url)
+                .NotEmpty()
+                .WithMessage("URL is required.")
+                .MaximumLength(2000)
+                .WithMessage("URL cannot exceed 2000 characters.")
+                .Must(BeAValidUrl)
+                .WithMessage("URL must be a valid HTTP or HTTPS URL.");
+
+            RuleFor(x => x.Type)
+                .IsInEnum()
+                .WithMessage($"Invalid media type. Valid values: {string.Join(", ", Enum.GetNames<MediaType>())}.");
+        }
+
+        private static bool BeAValidUrl(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                return false;
+
+            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+                   && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
