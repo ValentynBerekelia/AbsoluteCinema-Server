102c0d80928938490f9af4b73d0a0eb1820175f0
Valentyn Berekelia <yamato.watanabu@gmail.com>
2026-01-31 00:12:04 +0200

    Create post method

--- CinemaAura/AbsoluteCinema.Application/DTOs/Session/Session.Dto.cs
+++ CinemaAura/AbsoluteCinema.Application/DTOs/Session/Session.Dto.cs
@@ -1,3 +1,4 @@
+using AbsoluteCinema.Application.DTOs.Hall;
 using AbsoluteCinema.Domain.Entities;
 
 namespace AbsoluteCinema.Application.DTOs;
@@ -5,5 +6,7 @@ namespace AbsoluteCinema.Application.DTOs;
 public record SessionDto(
     SessionId Id,
     DateTime startDateTime,
-    MovieFormat? Format
-){}
+    MovieFormat? Format,
+    HallDto Hall
+)
+{}
