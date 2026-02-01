80e0554c14f24d9448d101c0436b859350b026fa
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2026-02-01 20:07:05 +0200

    Update SessionDto.cs

--- CinemaAura/AbsoluteCinema.Application/DTOs/Session/SessionDto.cs
+++ CinemaAura/AbsoluteCinema.Application/DTOs/Session/SessionDto.cs
@@ -4,7 +4,7 @@ using AbsoluteCinema.Domain.Entities;
 namespace AbsoluteCinema.Application.DTOs;
 
 public record SessionDto(
-    SessionId Id,
+    Guid Id,
     DateTime startDateTime,
     MovieFormat? Format
 )
