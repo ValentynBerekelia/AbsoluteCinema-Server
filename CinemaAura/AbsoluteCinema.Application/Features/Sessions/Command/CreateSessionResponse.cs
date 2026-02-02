6590e9b37fa26ae4c2dde0122fd7b582ac8f0f75
Hetmanska Oleksandra <w.getmanssska@gmail.com>
2026-02-02 15:56:21 +0200

    Created session features (#161)
    * Create CreateSessionCommandHandler.cs
    
    * Create CreateSessionCommandValidator.cs
    
    * Create CreateSessionResponse.cs
    
    * Create DeleteSession.cs
    
    * Create GetSessionDetails.cs
    
    * Create GetSessionList.cs
    
    * Update SessionController.cs

---
+++ CinemaAura/AbsoluteCinema.Application/Features/Sessions/Command/CreateSessionResponse.cs
@@ -0,0 +1,2 @@
+namespace AbsoluteCinema.Application.Features.Sessions.Commands.CreateSession;
+public record CreateSessionResponse(Guid Id);
---
+++ CinemaAura/AbsoluteCinema.Application/Features/Sessions/Command/DeleteSession.cs
@@ -0,0 +1,34 @@
+using AbsoluteCinema.Application.Repository;
+using AbsoluteCinema.Domain.Entities;
+using AbsoluteCinema.Domain.Exceptions;
+using MediatR;
+
+namespace AbsoluteCinema.Application.Features.Sessions.Commands.DeleteSession;
+
+public class DeleteSessionCommandHandler : IRequestHandler<DeleteSessionCommand>
+{
+    private readonly ISessionRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteSessionCommandHandler(ISessionRepository repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(DeleteSessionCommand request, CancellationToken cancellationToken)
+    {
+        var sessionId = new SessionId(request.SessionId);
+
+        var session = await _repository.GetByIdAsync(sessionId, cancellationToken);
+        if (session is null)
+        {
+            throw new DomainException("Session not found");
+        }
+
+        await _repository.DeleteAsync(sessionId, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
+
+public record DeleteSessionCommand(Guid SessionId) : IRequest;
