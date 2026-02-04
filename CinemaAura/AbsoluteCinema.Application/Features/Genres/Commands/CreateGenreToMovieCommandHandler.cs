e4454b39502b490cba6fec6682a836e15550032c
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2026-02-04 20:09:54 +0200

    Feature/genre (#185)
    * Fix merge conflicts in migrations and DbContext
    
    * delete unused migration
    
    * Fix merge conflicts in migrations and DbContext
    
    * delete unused migration
    
    * Add genre/get/post/put/delete
    
    * Attach/Detach/Create
    
    * Update appsettings.Development.json
    
    ---------
    
    Co-authored-by: Mykhailo Hordei <myhailo.gordei@gmail.com>

---
+++ CinemaAura/AbsoluteCinema.Application/Features/Genres/Commands/CreateGenreToMovieCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using AbsoluteCinema.Application.Repository;
+using AbsoluteCinema.Domain.Entities;
+using AbsoluteCinema.Domain.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteCinema.Application.Features.Genres.Commands
+{
+    public class CreateGenreToMovieCommandHandler(
+        IMovieRepository movieRepository,
+        IGenreRepository genreRepository,
+        IUnitOfWork unitOfWork) : IRequestHandler<CreateGenreToMovieCommand, CreateGenreToMovieResponse>
+    {
+        private readonly IMovieRepository _movie = movieRepository;
+        private readonly IGenreRepository _genre = genreRepository;
+        private readonly IUnitOfWork _unit = unitOfWork;
+        public async Task<CreateGenreToMovieResponse> Handle(CreateGenreToMovieCommand command, CancellationToken ct)
+        {
+            var genre = Genre.Create(command.GenreName);
+            await _genre.AddAsync(genre, ct);
+
+
+            var movieId = new MovieId(command.MovieId);
+            var movie = await _movie.GetByIdAsync(movieId, ct);
+            if(movie is null)
+            {
+                throw new DomainException($"Movie with id {command.MovieId} not found.");
+            }
+            movie.AddGenre(genre);
+            _movie.Update(movie);
+            await _unit.SaveChangesAsync(ct);
+            
+            return new CreateGenreToMovieResponse(genre.Id.Id, genre.Name);
+        }
+    }
+    public record CreateGenreToMovieCommand(
+        Guid MovieId,
+        string GenreName): IRequest<CreateGenreToMovieResponse>;
+    public record CreateGenreToMovieRequest(string Name);
+    
+    public record CreateGenreToMovieResponse(Guid GenreId,string Name);
+}
---
+++ CinemaAura/AbsoluteCinema.Domain/Specifications/MovieIncludeGenresSpec.cs
@@ -0,0 +1,22 @@
+﻿using AbsoluteCinema.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteCinema.Domain.Specifications
+{
+    public class MovieIncludeGenresSpec : Specification<Movie>
+    {
+        public MovieIncludeGenresSpec(MovieId movieId)
+        {
+            Criteria = m => m.Id == movieId;
+
+            // include medias
+            AddInclude(m=>m.Genres);
+
+            ApplyTracking(true);
+        }
+    }
+}
