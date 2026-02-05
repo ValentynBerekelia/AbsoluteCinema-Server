f1bc9e9abf9c85263999357b62ab733ec6a28d2d
Valentyn Berekelia <yamato.watanabu@gmail.com>
2026-02-05 16:21:15 +0200

    short tickets info

---
+++ CinemaAura/AbsoluteCinema.Application/Features/Tickets/Queries/GetTicketsFromSessionQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using AbsoluteCinema.Application.DTOs.Ticket;
+using AbsoluteCinema.Domain.Entities;
+using MediatR;
+
+namespace AbsoluteCinema.Application.Features.Tickets.Queries
+{
+    public class GetTicketsFromSessionQueryHandler(IGetTicketsFromSessionDtoQuery query)
+        : IRequestHandler<GetTicketsFromSessionQuery, List<GetTicketDetailsResponse>>
+    {
+        public async Task<List<GetTicketDetailsResponse>> Handle(GetTicketsFromSessionQuery request,CancellationToken ct)
+        {
+            var tickets = await query.ExecuteAsync(request, ct);
+            return tickets ?? new List<GetTicketDetailsResponse>();
+        }
+    }
+
+    public record GetTicketsFromSessionQuery : IRequest<List<GetTicketDetailsResponse>>
+    {
+        public SessionId SessionId { get; }
+        public GetTicketsFromSessionQuery(SessionId id) => SessionId = id;
+    }
+}
